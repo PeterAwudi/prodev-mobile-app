f7ddf54c265209272ce2902b5326875cefec83fd
PeterAwudi <awudipeter20@gmail.com>
2025-08-01 18:52:54 +0000

    Initial commit for prodev-mobile-app

---
+++ prodev-mobile-app-0x04/.eslintrc.js
@@ -0,0 +1,12 @@
+settings: {
+  'import/resolver': {
+    node: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      paths: ['.'],
+    },
+    alias: {
+      map: [['@', './']],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+  },
+}
